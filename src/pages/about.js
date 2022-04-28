699c3e97e0873b776dca3161bee8082034485a3b
amdyDiop <amdymila@gmail.com>
2022-04-28 12:41:35 +0000

    add new page

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Layout from "./components/layout";
+
+const AboutPage = () => {
+    return (
+
+        <Layout pageTitle={"About me"} children={"Hi, I'm amdy diop software developer full stack at SEPRO HOLDING"}>
+        </Layout>
+    )
+}
+export default AboutPage;
