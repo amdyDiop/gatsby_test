4d6f63e815c71cc6c7d9ad55435efec6f44de452
amdyDiop <amdymila@gmail.com>
2022-04-28 14:08:26 +0000

    undo change

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => {
         <Layout pageTitle="Home Page" children={"I'm making this by following the Gatsby Tutorial."}>
             <StaticImage
                 alt="anime"
-                src="./src/images/img3.jpg"
+                src="../images/img3.jpg"
             />
         </Layout>
     )
